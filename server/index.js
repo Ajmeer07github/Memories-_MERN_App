b0aa867b703794d75f9ff75dcec14ffcc17ceaed
Ajmeer07github <moudhajmeer2002@gmail.com>
2023-03-08 21:27:13 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -20,11 +20,11 @@ app.use('/user', userRoutes);
 
 const CONNECTION_URL=process.env.DB_CONNECTION_URL;
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 
-.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:5000`)))
+.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 
 .catch((error) => console.log(`${error} did not connect`));
 
